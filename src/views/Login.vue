be3c017ff2ad0308f38acc5f148e6861670e60b2
Dania99dev <dania99.dev@gmail.com>
2021-03-01 13:31:41 +0330

    responsiveness applied

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,8 +3,8 @@
     <div class="container">
       <img src="../assets/images/online-store.png" alt="online-store" />
       <div class="seperator"></div>
-      <h1>Sign in</h1>
       <form>
+        <h1>Sign in</h1>
         <div class="form-control">
           <label>Username</label>
           <input type="text" name="username" />
@@ -39,7 +39,6 @@ export default defineComponent({
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
   width: 100%;
   padding: 2rem;
-  margin: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -51,13 +50,18 @@ export default defineComponent({
   height: 1px;
   width: 25%;
   background-color: rgba(124, 44, 18, 0.3);
-  margin-bottom: 15px;
 }
 form {
   width: 100%;
   display: flex;
   flex-direction: column;
-  margin-top: 15px;
+  margin-top: 1rem;
+}
+form h1 {
+  text-align: center;
+  margin-bottom: 10px;
+  color: #c2410c;
+  font-weight: 800;
 }
 .form-control {
   font-size: 14px;
@@ -86,5 +90,45 @@ form button {
   background-color: #fdba74;
   padding: 12px;
   margin-top: 30px;
+  cursor: pointer;
+  transition-duration: 0.1s;
+}
+form button:hover {
+  background-color: #fed7aa;
+}
+
+@media (min-width: 640px) {
+  .login {
+    padding: 4rem 8rem;
+  }
+}
+
+@media (min-width: 1024px) {
+  .login {
+    display: flex;
+    align-items: center;
+  }
+  .container {
+    flex-direction: row;
+  }
+  .container img {
+    width: 42%;
+  }
+  form {
+    margin-top: 0;
+  }
+  form h1 {
+    text-align: left;
+  }
+  .seperator {
+    height: 5rem;
+    width: 1px;
+    margin: 0 2rem;
+  }
+}
+@media (min-width: 1280px) {
+  .login {
+    padding: 4rem 16rem;
+  }
 }
 </style>
