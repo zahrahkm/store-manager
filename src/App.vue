2c46484afebd565436197b2e70d0d72c30a1c509
Dania99dev <dania99.dev@gmail.com>
2021-03-02 17:50:51 +0330

    color palette seperated

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,4 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-
-// Variables
-$light-shades: #f0f1f1;
-$light-accent: #779c9c;
-$main: #10b981;
-$dark-accent: #6c6e6e;
-$dark-shades: #1e2027;
-$primary: #0ebf7f;
-$info: #1e2027;
-$success: #39b45e;
-$warning: #b7a426;
-$danger: #f44336;
 </style>
