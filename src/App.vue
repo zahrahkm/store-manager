073c32f0052b57b52998c9d98063201ec6033a67
Dania99dev <dania99.dev@gmail.com>
2021-03-02 16:07:37 +0330

    SCSS and color palette added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <router-view />
 </template>
-
-<style>
+<style lang="scss">
 @font-face {
   font-family: Mulish;
   src: url("./assets/fonts/Mulish-VariableFont_wght.ttf") format("truetype");
@@ -18,4 +17,16 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
+// Variables
+$light-shades: #f0f1f1;
+$light-accent: #779c9c;
+$main: #10b981;
+$dark-accent: #6c6e6e;
+$dark-shades: #1e2027;
+$primary: #0ebf7f;
+$info: #1e2027;
+$success: #39b45e;
+$warning: #b7a426;
+$danger: #f44336;
 </style>
